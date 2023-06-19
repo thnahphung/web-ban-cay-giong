e080a8600955e593857fa059c9fcb2cc508e2ded
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2023-06-19 22:37:40 +0700

    up

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,132 +1,44 @@
-import {useEffect} from "react";
-import CategoryApi from "../api/CategoryApi";
-import {loadCategories} from "../store/Action";
-import {useDispatch, useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
-    const categories = useSelector(state => state.categories);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const fetchCategories = async () => {
-            const data = await CategoryApi.getAll();
-            dispatch(loadCategories(data))
-        }
-        fetchCategories().catch(console.error);
-    }, [])
     return (
-        <footer className="page-footer font-small blue-grey lighten-5 mt-4">
-
-            <div className='default-color'>
-                <div className="container">
-
-                    <div className="row py-4 d-flex align-items-center">
-
-                        <div className="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-                            <h6 className="mb-0">Kết nối với chúng tôi thông qua mạng xã hội</h6>
-                        </div>
-
-
-                        <div className="col-md-6 col-lg-7 text-center text-md-right">
-
-
-                            <a className="fb-ic">
-                                <i className="fab fa-facebook-f white-text mr-4"> </i>
-                            </a>
-
-                            <a className="tw-ic">
-                                <i className="fab fa-twitter white-text mr-4"> </i>
-                            </a>
-
-                            <a className="gplus-ic">
-                                <i className="fab fa-google-plus-g white-text mr-4"> </i>
-                            </a>
-
-                            <a className="li-ic">
-                                <i className="fab fa-linkedin-in white-text mr-4"> </i>
-                            </a>
-
-                            <a className="ins-ic">
-                                <i className="fab fa-instagram white-text"> </i>
-                            </a>
-
-                        </div>
-
-
-                    </div>
-
-
-                </div>
-            </div>
-
-
-            <div className="container text-center text-md-left mt-5">
-
-
-                <div className="row mt-3 dark-grey-text">
-
-
-                    <div className="col-md-3 col-lg-4 col-xl-3 mb-4">
-
-
-                        <h6 className="text-uppercase font-weight-bold">Về công ty</h6>
-                        <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: "60px"}}/>
-                        <p>Chúng tôi tự hào giới thiệu đến bạn một trải nghiệm tuyệt vời trong việc mua sắm cây giống
-                            trực tuyến. Trang web của chúng tôi cung cấp một loạt các loại cây giống đa dạng và chất
-                            lượng.</p>
-
-                    </div>
-
-                    <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                        <h6 className="text-uppercase font-weight-bold">Sản phẩm</h6>
-                        <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: "60px"}}/>
-                        {categories.map(category => {
-                            return (<p key={category.id}>
-                                <Link className="dark-grey-text" href="#"
-                                      to={`/${category.urlParam}`}>{category.name}</Link>
-                            </p>)
-                        })}
-                    </div>
-
-                    <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                        <h6 className="text-uppercase font-weight-bold">Hữu ích</h6>
-                        <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: "60px"}}/>
+        <MDBFooter color="blue" className="font-small pt-4 mt-4 default-color">
+            <MDBContainer fluid className="text-center text-md-left">
+                <MDBRow>
+                    <MDBCol md="6">
+                        <h5 className="title">Footer Content</h5>
                         <p>
-                            <a className="dark-grey-text" href="#">Help</a>
+                            Here you can use rows and columns here to organize your footer
+                            content.
                         </p>
-
-                    </div>
-
-                    <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-
-                        <h6 className="text-uppercase font-weight-bold">Liên hệ</h6>
-                        <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: "60px"}}/>
-                        <p>
-                            <i className="fas fa-home mr-3"></i> Quận 9, Thủ Đức, Hồ Chí Minh</p>
-                        <p>
-                            <i className="fas fa-envelope mr-3"></i> bancaygiong@gmail.com</p>
-                        <p>
-                            <i className="fas fa-phone mr-3"></i> + 84 123456</p>
-
-
-                    </div>
-
-                </div>
-
+                    </MDBCol>
+                    <MDBCol md="6">
+                        <h5 className="title">Links</h5>
+                        <ul>
+                            <li className="list-unstyled">
+                                <a href="#!">Link 1</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Link 2</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Link 3</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Link 4</a>
+                            </li>
+                        </ul>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+            <div className="footer-copyright text-center py-3">
+                <MDBContainer fluid>
+                    &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+                </MDBContainer>
             </div>
-
-            <div className="footer-copyright text-center text-black-50 py-3">© 2020 Copyright:
-                <a className="dark-grey-text" href="/"> MDBootstrap.com</a>
-            </div>
-
-
-        </footer>
-
-
-    )
+        </MDBFooter>
+    );
 }
 
-export default Footer
+export default Footer;
